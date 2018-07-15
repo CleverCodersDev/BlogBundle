b5f916f28e9b7106b8920fd6e70415748c05b059
Zachary Quintana <zquintana@zingleme.com>
2018-07-14 18:06:44 -0700

    Add bundle.

---
+++ src/CleverCodersBlogBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CleverCoders\Bundle\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class CleverCodersBlogBundle
+ */
+class CleverCodersBlogBundle extends Bundle
+{
+
+}
